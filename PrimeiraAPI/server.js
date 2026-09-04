33ef9bd6c70d8c18cce11692f8ccd8e339ec1ef0
ArthurBackes09 <03219228089@senacrs.edu.br>
2026-09-04 11:28:34 -0300

    Primeira Api atrasada

--- PrimeiraAPI/server.js
+++ PrimeiraAPI/server.js
@@ -11,6 +11,31 @@ app.get('/', (req, res) => {
   res.send('Api funcionando');
 });
 
+app.get('/usuarios', (req, res) => {
+  const usuarios = [
+    { id: 1, nome: 'Arthur' },
+  ];
+  res.json(usuarios);
+});
+
+app.get('/eletronicos', (req, res) => {
+  const eletronicos =[
+    { id: 1, nome: 'Celular',},
+    { id: 2, nome: 'Notebook',},
+    { id: 3, nome: 'Tablet', }
+  ];
+  res.json(eletronicos);
+});
+
+app.post('/usuarios', (req, res) => {
+  const novousuario = req.body;
+
+  res.status(201).json({
+    message: 'Usuário criado com sucesso',
+    dados: novousuario
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando na porta http://localhost:${port}/`);
 });
